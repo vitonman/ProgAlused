0d58c38bdc01e5d7c952d860097a6ab0c84aa520
vitodon <creoxev@gmail.com>
2025-01-09 15:03:48 +0200

    methodcall2

---
+++ MethodCall2/MethodCall2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MethodCall2
+{
+    internal class Program
+    {
+
+        static bool paarisarvCheck(int number)
+        {
+            if ((number%2) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kas number on paaris või paaritu?");
+
+            int number = Int32.Parse(Console.ReadLine());
+
+            //check and call method
+            bool checkedNumber = paarisarvCheck(number);
+            
+            if(checkedNumber == false)
+            {
+                Console.WriteLine("number on paaritu");
+            }
+            else
+            {
+                Console.WriteLine("number on paaris");
+            }
+
+        }
+    }
+}
