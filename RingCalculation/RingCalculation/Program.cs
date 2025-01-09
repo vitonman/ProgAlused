dbbc44c53c5175dfe78d742ee1ead8e07b22accc
vitodon <creoxev@gmail.com>
2025-01-09 13:58:59 +0200

    add 2 tundi

---
+++ RingCalculation/RingCalculation/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RingCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Ruudu sees asub reng. Ringi raadius on 3.
+            //Leia ja väljasta ekraanile ruudu pindala ja ruudu ümbermõõt
+
+            Console.WriteLine("Sisesta palun raadius: ");
+
+            string r = Console.ReadLine();
+
+            double d = double.Parse(r); // diametr
+
+            double ruutS = d * d; 
+            double ruutP = d + d + d + d;
+
+            Console.WriteLine("Ruudu S: " + ruutS);
+            Console.WriteLine("Ruudu P: " + ruutP);
+
+            //ringi pindala, ringi ümbermõõt
+
+            Console.WriteLine("Ringi pindala: " + Math.PI * (d));
+            Console.WriteLine("Ringi ümbermõõt: " + 2 * Math.PI + r);
+
+
+
+
+
+        }
+    }
+}
---
+++ ifelseagain/ifelseagain/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ifelseagain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Please enter your age: ");
+            string number = Console.ReadLine();
+
+            int age = Int32.Parse(number);
+
+            if (age <= 18 && age! < 1)
+            {
+                Console.WriteLine("Your age below 18");
+            }
+            else if (age >= 19 && age <= 65)
+            {
+                Console.WriteLine("Adult");
+            }
+            else if (age >= 65)
+            {
+                Console.WriteLine("pension");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong");
+            }
+            {
+
+            }
+        }
+    }
+}
