e06f10064a7775bf14276599c1279a47b990beff
vitodon <creoxev@gmail.com>
2025-01-09 14:51:40 +0200

    meetodi kutsumine

---
+++ MeetodiKutsumine/MeetodiKutsumine/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MeetodiKutsumine
+{
+    internal class Program
+    {
+
+        static void jalgRattas()
+        {
+            System.Console.WriteLine("Jalgrattas Jalgrattas Jalgrattas");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            jalgRattas();
+        }
+
+    
+    }
+}
