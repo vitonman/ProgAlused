00f36d49fef39eacee44667c107067067e4f5528
vitodon <creoxev@gmail.com>
2025-01-10 10:35:03 +0200

    daytimeSwitch

---
+++ lastSwitch/lastSwitch/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace lastSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            int day = (int)DateTime.Now.DayOfWeek;
+
+            
+
+
+            switch (day) {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Thuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+
+        }
+    }
+}
