748c941481f6ca6ec10e7faa13dc224ba27013db
vitodon <creoxev@gmail.com>
2025-01-10 11:02:40 +0200

    last

---
+++ CoffeePrice/CoffeePrice/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CoffeePrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Spundi kohvik!");
+            Console.WriteLine("\n\nMenü (kohvi valik)\n\n");
+            Console.WriteLine("1. Väike tass");
+            Console.WriteLine("2. Keskmine tass");
+            Console.WriteLine("3. Suur tass");
+            Console.WriteLine("\n\nPalun tee oma valik\n\n");
+
+            byte coffeSize = byte.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            switch (coffeSize)
+            {
+                case 1:
+                    price = 1.5;
+                    break;
+                case 2:
+                    price = 2.5;
+                    break;
+                case 3:
+                    price = 3;
+                    break;
+            }
+
+            Console.
+                WriteLine(price);
+        }
+    }
+}
