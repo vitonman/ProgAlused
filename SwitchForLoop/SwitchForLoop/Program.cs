ec9b867bc5717c64bdc8b5a8cf28d176c6fb7f4f
vitodon <creoxev@gmail.com>
2025-01-10 09:55:44 +0200

    10.01.2025(switchloopetc)

---
+++ SwitchForLoop/SwitchForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SwitchForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (c)
+            {
+                case Colour.Red:
+                    Console.BackgroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 130; i++)
+            {
+
+            }
+        }
+    }
+    
+    
+}
---
+++ SwitchLeppeks/SwitchLeppeks/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SwitchLeppeks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write please a, y or u");
+
+
+            //rakendus tuleb, et sisesta vokaal
+            //3 vokaalist piisab
+            //kui vokaal on sisestatud, siis ütleb, et sisestasid selle tähe, kasuta switchi
+
+
+            const string a = " You pressed a";
+            const string y = " You pressed y";
+            const string u = " You pressed u";
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "a": 
+                    writeSentence(a);
+                    break;
+
+                case "y": 
+                    writeSentence(y);
+                    break;
+
+                case "u": 
+                    writeSentence(u);
+                    break;
+
+                default:
+                    Console.WriteLine(" You did not type a, u or y");
+                    Console.WriteLine();
+                    Console.ReadLine();
+                    break;
+            }
+
+
+        }
+        static void writeSentence(string a)
+        {
+            Console.WriteLine(a); // correction 3
+        }
+    }
+      
+}
