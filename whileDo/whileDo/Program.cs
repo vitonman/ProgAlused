54885edf5a1037c2148220696d6dd2abe27df6e6
vitodon <creoxev@gmail.com>
2025-01-16 16:24:27 +0200

    last

---
+++ whileDo/whileDo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace whileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("While and DO");
+
+            int number = 1;
+
+            do
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+            while (number !< 6);
+        }
+    }
+}
