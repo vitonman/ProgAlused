9fbf599ac501e13059fa891057121ee45d3458a2
vitodon <creoxev@gmail.com>
2025-01-30 15:29:40 +0200

    30.01.25

---
+++ ForTriangleNumbers/ForTriangleNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ForTriangleNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+
+            Console.WriteLine("Sisesta ridade arv: ");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    if(i < 10)
+                    {
+                        Console.Write(i + "  ");
+                    }
+                    else
+                    {
+                        Console.Write(i + " ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ for4for/for4for/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace for4for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < rows; i++)
+            {
+        
+                for (int j = 0; j< rows; j++) {
+
+                    Console.Write("{0} ");
+
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
