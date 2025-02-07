8a59fae9fc687d46283cc4bceeb9a4287573d4a0
vitodon <creoxev@gmail.com>
2025-02-07 11:38:21 +0200

    DecToBin

---
+++ DecToBin/DecToBin/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DecToBin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimal to binary");
+
+            int n1;
+            string binaryNumber = "";
+
+            Console.WriteLine("Input First Number: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            while (n1 > 0)
+            {
+                int reminder = n1 % 2;
+                binaryNumber = reminder + binaryNumber;
+                n1 /= 2;
+            }
+            Console.WriteLine(binaryNumber);
+        }
+    }
+}
