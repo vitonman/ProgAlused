b8d8abf4823acad20a4e311dd20f5b8a55f62605
vitodon <creoxev@gmail.com>
2025-02-07 11:09:02 +0200

    07.02.25

---
+++ FimonacchiAlg/FimonacchiAlg/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FimonacchiAlg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Display forst n term of Fobonacchi series");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Input number of terms to be displayed: ");
+
+            int prv = 0, pre = 1, trm, i, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0}     {1}", prv, pre);
+
+            for (i = 3; i <= n; i++)
+            {
+                trm = prv + pre;
+                Console.Write("   {0}   ", trm);
+                prv = pre;
+                pre = trm;
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ maslou/maslou/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace maslou
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            numberPyramid(5);
+
+        }
+
+        static void numberPyramid(int rows)
+        {
+            int i, j;
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= rows-1; j++)
+                {
+
+                    Console.Write(" ");
+                }
+                for (g = 1; g <= i; g++)
+                {
+
+                    Console.Write();
+                }
+
+            }
+        }
+    }
+
+
+}
+
+
