b971f477a4de19bd7e88d9372cf12b0b125ea46d
vitodon <creoxev@gmail.com>
2025-02-07 11:49:08 +0200

    varvar

---
+++ ForEachList/ForEachList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace ForEachList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ForEach List");
+
+            var numbers = new List<int>() {5, -8, 3, 15, 9, 17, 0, 4 };
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine("Sum {0}", sum);
+            Console.WriteLine();
+        }
+    }
+}
